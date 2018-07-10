6144b38f8fa3a166f440028bb6595b1193054e14
邹志刚 <>
2018-07-10 14:58:44 +0800

    Add 下拉刷新加载更多示例

--- app/src/main/java/pulltorefresh/zhigang/com/recyclerviewpulltorefresh/MainActivity.java
+++ app/src/main/java/pulltorefresh/zhigang/com/recyclerviewpulltorefresh/MainActivity.java
@@ -8,24 +8,39 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+
 import java.util.ArrayList;
 
+import pulltorefresh.zhigang.com.recyclerviewpulltorefresh.pulltorefresh.PtrDefaultHandler;
+import pulltorefresh.zhigang.com.recyclerviewpulltorefresh.pulltorefresh.PtrFrameLayout;
 import pulltorefresh.zhigang.com.recyclerviewpulltorefresh.pulltorefresh.PullToRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
     PullToRefreshLayout ptrLayout;
     RecyclerView recyclerView;
     static Activity mBaseActivity;
+    RecyclerviewAdapter recyclerviewAdapter;
     private static final String[] TITLE = {"Animation", "MultipleItem", "Header/Footer", "PullToRefresh", "Section", "EmptyView", "DragAndSwipe", "ItemClick", "ExpandableItem", "DataBinding", "UpFetchData"};
     private ArrayList<String> mDataList;
+    int mPage = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mBaseActivity = this ;
-        initView();
+        mBaseActivity = this;
         initData();
+        initView();
+
+    }
+
+    private void initData() {
+        mDataList = new ArrayList<>();
+        for (int i = 0; i < TITLE.length; i++) {
+            mDataList.add(TITLE[i]);
+        }
     }
 
     public void initView() {
@@ -33,11 +48,11 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(mBaseActivity));
         recyclerView.setEnabled(true);
-        myOrderAdapter = new MyOrderAdapter(mBaseActivity, R.layout.layout_order_item, mDataList);
-        recyclerView.setAdapter(myOrderAdapter);
-        myOrderAdapter.setEnableLoadMore(true);
+        recyclerviewAdapter = new RecyclerviewAdapter(mBaseActivity, R.layout.layout_order_item, mDataList);
+        recyclerView.setAdapter(recyclerviewAdapter);
+        recyclerviewAdapter.setEnableLoadMore(true);
         //加载更多
-        myOrderAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
+        recyclerviewAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
             @Override
             public void onLoadMoreRequested() {
                 inView();
@@ -51,21 +66,16 @@ public class MainActivity extends AppCompatActivity {
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
+                        mPage = 1;
                         ptrLayout.refreshComplete();
 
                     }
-                }, 3000);
+                }, 1500);
 
             }
         });
     }
 
-    private void initData() {
-        mDataList = new ArrayList<>();
-        for (int i = 0; i < TITLE.length; i++) {
-            mDataList.add(TITLE[i]);
-        }
-    }
 
     public void inView() {
 //                        myOrderAdapter.loadMoreFail(); //加载失败
@@ -74,13 +84,21 @@ public class MainActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                mPage++;
+                if (mPage == 3) {
+                    ptrLayout.refreshComplete();
+                    recyclerviewAdapter.loadMoreEnd();
+                    recyclerviewAdapter.addData(mDataList);
+                    return;
+                }
                 ptrLayout.refreshComplete();
-                myOrderAdapter.loadMoreComplete();
-                myOrderAdapter.addData(mDataList);
+                recyclerviewAdapter.loadMoreComplete();
+                recyclerviewAdapter.addData(mDataList);
                 Log.d(TAG, "LoadMore-----");
 
+
             }
-        }, 3000);
+        }, 1500);
 
     }
 }
